ba375ba4f9e09e9baa3f7f6b3d1b5099392b959e
cyberwolf888 <wijaya.imd@gmail.com>
2016-05-20 18:13:55 +0800

    V.1.2

--- app/src/main/java/com/fifgroup/penagihan/fifgroup/MainMenu.java
+++ app/src/main/java/com/fifgroup/penagihan/fifgroup/MainMenu.java
@@ -1,18 +1,47 @@
 package com.fifgroup.penagihan.fifgroup;
 
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.content.Intent;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainMenu extends AppCompatActivity {
+
+    // URL to get contacts JSON
+    private static String url = "http://www.hendrawijaya.xyz/mobile/list.php";
+
+    // JSON Node names
+    private static final String TAG_STUDENTINFO = "studentsinfo";
+    private static final String TAG_ID = "id";
+    private static final String TAG_NAME = "name";
+    private static final String TAG_EMAIL = "email";
+    private static final String TAG_ADDRESS = "address";
+    private static final String TAG_GENDER = "gender";
+    private static final String TAG_PHONE = "phone";
+    private static final String TAG_PHONE_MOBILE = "mobile";
+    private static final String TAG_PHONE_HOME = "home";
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,16 +49,117 @@ public class MainMenu extends AppCompatActivity {
         setContentView(R.layout.activity_main_menu);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        /*
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+        listView = (ListView) findViewById(R.id.mobile_list);
+
+        //get request
+        new GetList().execute();
+
+    }
+
+    //JSON handler
+    private class GetList extends AsyncTask<Void, Void, Void> {
+
+        // Hashmap for ListView
+        ArrayList<HashMap<String, String>> studentList;
+        ProgressDialog pDialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // Showing progress dialog
+            pDialog = new ProgressDialog(MainMenu.this);
+            pDialog.setMessage("Please wait...");
+            pDialog.setCancelable(false);
+            pDialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+
+            // Creating service handler class instance
+            WebRequest webreq = new WebRequest();
+
+            // Making a request to url and getting response
+            String jsonStr = webreq.sendGetRequest(url);
+
+            Log.d("Response: ", "> " + jsonStr);
+
+            studentList = ParseJSON(jsonStr);
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            // Dismiss the progress dialog
+            if (pDialog.isShowing())
+                pDialog.dismiss();
+            /**
+             * Updating parsed JSON data into ListView
+             * */
+            if(studentList != null){
+                ListAdapter adapter = new SimpleAdapter(
+                        MainMenu.this, studentList,
+                        R.layout.list_item, new String[]{TAG_NAME, TAG_EMAIL,
+                        TAG_PHONE_MOBILE}, new int[]{R.id.name,
+                        R.id.email, R.id.mobile});
+
+                listView.setAdapter(adapter);
             }
-        });
-        */
+
+        }
+
+    }
+
+    private ArrayList<HashMap<String, String>> ParseJSON(String json) {
+        if (json != null) {
+            try {
+                // Hashmap for ListView
+                ArrayList<HashMap<String, String>> studentList = new ArrayList<HashMap<String, String>>();
+
+                JSONObject jsonObj = new JSONObject(json);
+
+                // Getting JSON Array node
+                JSONArray students = jsonObj.getJSONArray(TAG_STUDENTINFO);
+
+                // looping through All Students
+                for (int i = 0; i < students.length(); i++) {
+                    JSONObject c = students.getJSONObject(i);
+
+                    String id = c.getString(TAG_ID);
+                    String name = c.getString(TAG_NAME);
+                    String email = c.getString(TAG_EMAIL);
+                    String address = c.getString(TAG_ADDRESS);
+                    String gender = c.getString(TAG_GENDER);
+
+                    // Phone node is JSON Object
+                    JSONObject phone = c.getJSONObject(TAG_PHONE);
+                    String mobile = phone.getString(TAG_PHONE_MOBILE);
+                    String home = phone.getString(TAG_PHONE_HOME);
+
+                    // tmp hashmap for single student
+                    HashMap<String, String> student = new HashMap<String, String>();
+
+                    // adding each child node to HashMap key => value
+                    student.put(TAG_ID, id);
+                    student.put(TAG_NAME, name);
+                    student.put(TAG_EMAIL, email);
+                    student.put(TAG_PHONE_MOBILE, mobile);
+
+                    // adding student to students list
+                    studentList.add(student);
+                }
+                return studentList;
+            } catch (JSONException e) {
+                e.printStackTrace();
+                return null;
+            }
+        } else {
+            Log.e("ServiceHandler", "Couldn't get any data from the url");
+            return null;
+        }
     }
 
 
